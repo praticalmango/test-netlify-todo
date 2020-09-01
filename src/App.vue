9ac06473f130797103f5ace859c7701bfedead1d
Bastien Franck <franckbastien04@gmail.com>
2020-09-01 17:58:41 +0200

    fin gestion todo via todo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Todos v-bind:todos="todos" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
@@ -18,12 +18,12 @@ export default {
         {
           id: 1,
           title: "Todo One",
-          completed: false,
+          completed: true,
         },
         {
           id: 2,
           title: "Todo Two",
-          completed: false,
+          completed: true,
         },
         {
           id: 3,
@@ -33,6 +33,11 @@ export default {
       ],
     };
   },
+  methods: {
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
+  }
 };
 </script>
 
